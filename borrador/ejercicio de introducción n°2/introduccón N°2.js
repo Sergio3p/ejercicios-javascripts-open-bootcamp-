a6f2865708dba3604d443f66415da6f13ecf71b9
sergio3p <panasergiopacheco@gmail.com>
2022-10-13 12:25:03 -0300

    primer repositorio

---
+++ borrador/ejercicio de introducción n°2/introduccón N°2.js
@@ -0,0 +1,40 @@
+
+//string - nombre:
+"Me llamo sergio";
+
+//con constante:
+
+const nombre = "Me llamo sergio";
+nombre
+
+//number - edad:
+20;
+//con var o let:
+
+const edad = 20;
+edad
+
+//boolean - ¿Eres desarrollador?
+true;
+
+//boolean 
+
+if (true){
+    console.log("soy programador");
+} else {
+    console.log("no soy desarrollador");
+}
+
+//Date - Fecha de nacimiento:
+const fecha_cadena = ("december 17 2001")
+
+
+console.log(fecha_cadena) 
+//Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+
+const libro = {
+    titulo: "La noche de la usina",
+    autor: "Eduardo Sacheri",
+    url: "https://www.academia.edu/35137930/La_noche_de_la_usina_de_Eduardo_Sacheri_pdf",
+    fecha: "mayo del 2016"
+}
